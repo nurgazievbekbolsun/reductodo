f53cc2d5c5dfa201fbe0f533e19616b5f40288ce
Beku001 <nurgazevbekbolsun906@gmail.com>
2023-08-15 13:06:09 +0600

    ref

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,23 +1,28 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { addText, delText } from "../../redux/action";
 import { useDispatch, useSelector } from "react-redux";
 
 const Home = () => {
   const [value, setValue] = useState("");
   // const [value1, setValue1] = useState("");
+  const divRef = useRef('nkl');
   const { todo } = useSelector((s) => s);
   const dispatch = useDispatch();
-const enter = (e) =>{
-  if(e.key === 'Enter'){
-    dispatch(addText(value)) 
-  }
-}
+  const enter = (e) => {
+    if (value.trim() !== "" && e.key === "Enter") {
+      dispatch(addText(value));
+      setValue("");
+    }
+  };
   return (
     <div className="container w-[1140px] mx-auto">
       <div className="flex flex-col justify-center items-center gap-4 pt-4 bg-white">
         <input
-        onKeyDown={enter}
-          onChange={(e) => setValue(e.target.value)}
+          onKeyDown={enter}
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
           type="search"
           id="default-search"
           className=" p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -34,12 +39,20 @@ const enter = (e) =>{
           placeholder="img"
           required
         /> */}
-
+  <h1 ref={divRef}></h1>
         <button
-          onClick={() => {
-            dispatch(addText(value));
-            // dispatch(addText(value1));
-          }}
+          onClick={() => value === '' ? divRef : 
+          (dispatch(addText(value)),setValue(''))
+          //   {
+          //   if (value === "") {
+          //     return divRef
+           
+          //   } else if (value.trim() !== "") {
+          //     dispatch(addText(value));
+          //     setValue("");
+          //   }
+          // }
+        }
           type="button"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
         >
@@ -47,19 +60,25 @@ const enter = (e) =>{
         </button>
       </div>
 
-      <div className="rounded px-2
-       bg-gray-400  mt-10 mx-auto w-[400px] h-[auto] ">
-        {todo?.map(
-          (el) => (
+      <div
+        className="rounded px-2
+       bg-gray-400  mt-10 mx-auto w-[400px] h-[auto] "
+      >
+        {todo?.map((el) => (
+          <div>
             <div className="flex justify-between items-center">
               <h1>{el.text}</h1>
-              <button onClick={() =>{
-                dispatch(delText(el))
-                }}>delete</button>
-                {/* <img src={el.image} alt="" /> */}
+              <button
+                onClick={() => {
+                  dispatch(delText(el));
+                }}
+              >
+                delete
+              </button>
+              {/* <img src={el.image} alt="" /> */}
             </div>
-          )
-        )}
+          </div>
+        ))}
       </div>
     </div>
   );
